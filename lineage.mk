ebe175aa33e9c004989f2dd96580e6d8f05fc91c
synt4x93 <synt4x.93@gmail.com>
2019-04-16 15:10:44 +0000

    crownlte: add fingerprint override to pass cts

--- lineage.mk
+++ lineage.mk
@@ -11,3 +11,9 @@ PRODUCT_NAME := lineage_crownlte
 # fix improper s-pen rotation
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/files/samsung-sec_e-pen.idc:system/usr/idc/sec_e-pen.idc
+
+# add fingerprint override to pass cts
+BUILD_FINGERPRINT := samsung/crownltexx/crownlte:8.1.0/M1AJQ/N960FXXS2ASB1:user/release-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+        PRIVATE_BUILD_DESC="crownltexx-user 8.1.0 M1AJQ N960FXXS2ASB1 release-keys"
